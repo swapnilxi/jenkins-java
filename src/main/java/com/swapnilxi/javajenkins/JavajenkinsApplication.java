7b897afb85d22b187d1e95eb8c5ccd36cdc2f38d
swapnil <swapnilg671@gmail.com>
2023-05-08 14:39:43 +0530

    java jenkins

---
+++ src/main/java/com/swapnilxi/javajenkins/JavajenkinsApplication.java
@@ -0,0 +1,14 @@
+package com.swapnilxi.javajenkins;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavajenkinsApplication {
+
+	public static void main(String[] args) {
+		System.out.println("java jenkins application");
+		SpringApplication.run(JavajenkinsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/swapnilxi/javajenkins/JavajenkinsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.swapnilxi.javajenkins;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavajenkinsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
